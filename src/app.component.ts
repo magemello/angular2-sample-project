28cdf4cf70ab3d481e08b4d16f60c148654c8425
Mario Romano <dnamario@hotmail.com>
2016-05-24 16:58:08 +0100

    add first component

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HelloWorld } from './hello-world.component';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello world</h1>`
+  template: `<h1><hello-world [name]="myname"></hello-world></h1>`,
+  directives: [HelloWorld]
 })
 export class AppComponent {
+
+  myname:string = 'Mario Romano';
 }
