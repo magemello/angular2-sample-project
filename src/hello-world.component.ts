8e7cfe923fc833346fa8d3d37cb5dae296047b33
Mario Romano <dnamario@hotmail.com>
2016-05-25 22:04:00 +0100

    add input

--- src/hello-world.component.ts
+++ src/hello-world.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
-import { HelloService } from './hello.service';
 import { HiDirective } from './hi.directive';
 import { ChangeColorDirective } from './changeColor.directive';
 
 @Component({
     selector: 'hello-world',
     template: `<div changeColor >
-                <h1>Hello world my name is {{name.value}}, from service
+                <h1>Hello world my name is {{name}}, from service
                       <p *hi="name">
                           Name is right.
                       </p>
                 </h1>
-              </div>`,
+              </div>
+              <input (keyup)="onKey($event)">`,
     directives: [HiDirective, ChangeColorDirective]
 })
 export class HelloWorld {
 
     name: string;
 
-    constructor(helloService: HelloService) {
-        this.name = helloService.getName();
+    onKey(event: KeyboardEvent) {
+        this.name = (<HTMLInputElement>event.target).value;
     }
+
 }
