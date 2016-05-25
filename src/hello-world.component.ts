6dadbb09a1ed35cbfeb9c21e182e941e6312f453
Mario Romano <dnamario@hotmail.com>
2016-05-25 21:58:38 +0100

    value

--- src/hello-world.component.ts
+++ src/hello-world.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
-import { HelloService } from './hello.service';
 import { HiDirective } from './hi.directive';
 
 @Component({
     selector: 'hello-world',
-    template: `<h1>Hello world my name is {{name.value}}, from service
-              <p *hi="name">
-                  Name is right.
-              </p>
-              </h1>`,
+    template: `<h1>Hello world my name is {{name}}, from service
+                      <p *hi="name">
+                          Name is right.
+                      </p>
+              </h1>
+              <input (keyup)="onKey($event)">`,
     directives: [HiDirective]
 })
 export class HelloWorld {
 
     name: string;
 
-    constructor(helloService: HelloService) {
-        this.name = helloService.getName();
+    onKey(event: KeyboardEvent) {
+        this.name = (<HTMLInputElement>event.target).value;
     }
+
 }
