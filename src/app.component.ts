08e8c6beb7a516d1805732eac9963ed5b0260be0
Mario Romano <dnamario@hotmail.com>
2016-06-17 23:45:50 +0100

    update html

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
+  styles: ['h1 { color: red; }'],
   template: `<h1>Hello world</h1>`
 })
 export class AppComponent {
